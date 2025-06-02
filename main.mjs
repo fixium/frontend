9ddfa16fee3bb13cfa01e27815eaa16e6f900232
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-06-01 22:38:35 -0700

    set fixium icon in window electron, change text 'Cargando' when finish

--- main.mjs
+++ main.mjs
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 import fs from 'fs';
 import https from 'https';
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url)); // Define __dirname
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 let mainWindow;
 let viteProcess;
@@ -100,7 +100,8 @@ app.on('ready', async () => {
             contextIsolation: true,
             nodeIntegration: false,
             disableHardwareAcceleration: true,
-        }
+        },
+        icon: path.join(__dirname, 'static', 'Fix.png')
     });
 
     mainWindow.setMenuBarVisibility(false);
@@ -111,9 +112,10 @@ app.on('ready', async () => {
     startVite();
 
     try {
-        await waitOn({ resources: ['http://localhost:5173'], timeout: 60000 });
+        await waitOn({ resources: ['http://localhost:5173'], timeout: 40000 });
         console.log('Vite está listo.');
         mainWindow.loadURL('http://localhost:5173');
+        mainWindow.setTitle('Fixium');
     } catch (err) {
         console.error('Error al esperar a Vite:', err.message);
         app.quit();
